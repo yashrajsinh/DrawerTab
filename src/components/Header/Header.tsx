bef3ac665755016f27e713bc4cb3b27c2055cfc3
yash <yashrajsinhjadeja2253@gmail.com>
2026-03-09 18:36:08 +0530

    Moment before disaster

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,11 +5,13 @@ const Header = () => {
   return (
     <View style={styles.container}>
       <Image
-        source={{ uri: 'https://i.pravatar.cc/150?img=12' }}
+        source={{
+          uri: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a0a7586b-3d38-4293-9d13-75e10782ff57/dgvmsg8-5606c4fa-ebbd-4d60-9a6a-0fb1dd864167.png/v1/fit/w_816,h_770,q_70,strp/gentleman_cillian_murphy_png_image_by_ongpng_dgvmsg8-414w-2x.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzcwIiwicGF0aCI6Ii9mL2EwYTc1ODZiLTNkMzgtNDI5My05ZDEzLTc1ZTEwNzgyZmY1Ny9kZ3Ztc2c4LTU2MDZjNGZhLWViYmQtNGQ2MC05YTZhLTBmYjFkZDg2NDE2Ny5wbmciLCJ3aWR0aCI6Ijw9ODE2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.CGzX1KwSi_VzPXpqJtZuAqKX0p2XJw8mtKvf_8mYXo8',
+        }}
         style={styles.avatar}
       />
 
-      <Text style={styles.greeting}>Good morning, John</Text>
+      <Text style={styles.greeting}>Good morning, Mr. Shelby</Text>
     </View>
   );
 };
@@ -19,8 +21,9 @@ export default Header;
 const styles = StyleSheet.create({
   container: {
     padding: 20,
-    backgroundColor: '#4F46E5',
+    backgroundColor: '#8494FF',
     alignItems: 'center',
+    borderRadius: 20,
   },
 
   avatar: {
